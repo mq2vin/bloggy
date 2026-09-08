829dd196abfa509559541368fcd5643e42009aa1
mq2vin <mcatan@iut.univ-paris8.fr>
2026-09-08 11:44:12 +0200

    FIX - suppresion de la variable user (model) dans le router

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userController from '../controllers/userController.js'
-import User from "../models/user.js";
+
 import UserController from "../controllers/userController.js";
 
 const router = express.Router();
